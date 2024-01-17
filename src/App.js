813b5a6888df4271fd0fe6fc3aea556fb9d9a35e
Shaswat <shaswatrajput7@gmail.com>
2024-01-17 23:42:49 +0530

    updated inputbox

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ function App() {
   return (
     <div className="container">
       <ul>
-        <li>Same tags can't be entered again</li>
-        <li>Press Tab, Enter, Comma to add a tag to the list</li>
-        <li>Tags can be suggested either if Name or Email matches</li>
-        <li>Press Backspace two times to fully remove a tag from the list</li>
+        <li>Same tags can't be entered again.</li>
+        <li>Press Tab, Enter, Comma to add a tag to the list.</li>
+        <li>Tags can be suggested either if Name or Email matches.</li>
+        <li>Press Backspace two times to fully remove a tag from the list.</li>
       </ul>
       <h1>Zepto Chips-</h1>
       <InputChips />
